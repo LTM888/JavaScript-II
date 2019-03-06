22bc0ddabd57912d3c8a72d81754ec550ba2206f
LTM888 <leigh.tm@gmail.com>
2019-03-05 22:01:21 -0500

    still working on callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -50,7 +50,7 @@ function showLast(array, callBack) {
 }
 console.log(showLast);
 
-
+showLast();
 // function sumNums(x, y, cb) {
 //   // sumNums adds two numbers (x, y) and passes the result to the callback.
 // }
